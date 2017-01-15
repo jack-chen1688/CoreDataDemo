19768536689f57cb7ece103584e50dcc6ec37f45
xuehua <xuehua@gmail.com>
2017-01-14 18:19:27 -0800

    Added codes on save and restore core data.

--- CoreDataDemo/ViewController.swift
+++ CoreDataDemo/ViewController.swift
@@ -7,12 +7,50 @@
 //
 
 import UIKit
+import CoreData
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        
+        let context = appDelegate.persistentContainer.viewContext
+        
+        let newUser = NSEntityDescription.insertNewObject(forEntityName: "Users", into: context)
+        
+        newUser.setValue("Bill", forKey: "username")
+        newUser.setValue("myPass", forKey: "password")
+        newUser.setValue(39, forKey: "age")
+        
+        do {
+            try context.save()
+            print("Saved successfully")
+        } catch  {
+            print("There was an error")
+        }
+        
+        let request = NSFetchRequest<NSFetchRequestResult>(entityName: "Users")
+        
+        request.returnsObjectsAsFaults = false
+        
+        do {
+            let results = try context.fetch(request)
+            
+            if results.count > 0 {
+                for result in results as! [NSManagedObject] {
+                    if let username = result.value(forKey: "username")  as? String {
+                        print("username is", username)
+                    }
+                }
+            } else {
+                print("no result")
+            }
+        } catch  {
+            print ("fetch error");
+        }
+        
     }
 
     override func didReceiveMemoryWarning() {
